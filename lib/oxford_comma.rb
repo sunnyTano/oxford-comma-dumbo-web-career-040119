bedef3288cd4c0a7ff468f60795ad8fd4cbab7d3
Susan Tan <ssusantan9@gmail.com>
2019-03-25 03:02:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
-def oxford_comma(array)
-
+def oxford_comma(arr)
+  if arr.length == 1
+    return arr.join(" ")
+  elsif arr.length == 2
+    return arr.join(" and ")
+  elsif arr.length >= 3
+    arr[-1] = "and #{arr[-1]}"
+    return arr.join(", ")
+  end
 end
