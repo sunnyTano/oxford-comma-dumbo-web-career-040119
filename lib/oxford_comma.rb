815d3eaf3b8ed50036be33d58aa2701f5247149e
Susan Tan <ssusantan9@gmail.com>
2019-03-25 03:00:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(arr)
   elsif arr.length == 2
     return arr.join(" and ")
   elsif arr.length >= 3
-    return "and #{ar[-1]}"
+    return "and #{arr[-1]}"
     return arr.join(", ")
   end
 end
